370426f96f835b7d2a06cf2f73be4bdf7e7582f9
dsaisho <dsaisho@gmail.com>
2024-08-16 00:24:46 -0700

    setup pattern for hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,1 +1,11 @@
-export { handle } from "./auth"
+import { handle as authHandle } from "./auth"
+import { sequence } from '@sveltejs/kit/hooks';
+
+const authorizationHandle = async ({ event, resolve })=>{
+    const session = await event.locals.auth();
+    console.log('sessiontest')
+    return resolve(event);
+}
+
+
+export const handle = sequence(authHandle, authorizationHandle)
