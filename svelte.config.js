647e5d811e55801289e32857d1478fa9daa100a3
dsaisho <dsaisho@gmail.com>
2024-08-29 19:17:04 -0700

    add basic react component to svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-node';
+import { sveltePreprocess } from 'svelte-preprocess';
+import preprocessReact from 'svelte-preprocess-react/preprocessReact';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+
+  },
+  preprocess: preprocessReact({
+    preprocess: sveltePreprocess({ sourceMap: true }),
+  }),
 };
 
 export default config;
